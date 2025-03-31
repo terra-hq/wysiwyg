486c1b8f03e6bdf22f4eada2185471a3b2976958
Amaia del Olmo <amaia@longneckandthunderfoot.com>
2025-03-31 10:21:07 +0200

    fix: 🐛 Fix lists margin-bottom.

--- src/js/main.ts
+++ src/js/main.ts
@@ -8,6 +8,21 @@ class TFCommon {
     }
 
     init() {
+
+        const startLists = document.querySelectorAll('ol[start]');
+
+        if (startLists.length > 0) {
+            startLists.forEach(ol => {
+                const startAttr = ol.getAttribute('start');
+                if (startAttr !== null) {
+                    const start = parseInt(startAttr, 10);
+                    if (!isNaN(start)) {
+                        (ol as HTMLElement).style.counterReset = `item ${start - 1}`;
+                    }
+                }
+            });
+        }
+
     }
 }
 export default TFCommon;
